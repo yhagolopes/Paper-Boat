f355fddb7678c6bf51d479cb257a1851f2e6e301
Yhago <yhagodev@gmail.com>
2023-06-21 14:55:31 -0300

    Add logic and draw boat

---
+++ README.md
@@ -0,0 +1,16 @@
+# Paper Boat
+
+This is my first project.\
+A simple game using Javascript, HTML and CSS.
+
+
+## Screenshots
+
+![Screenshot](screenshots/1.jpeg)
+
+## Demo
+[Click here]()
+
+## License
+
+[MIT](LICENSE.txt)
--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-import Game from "./js/game.js";
+import Game from "./src/game.js";
 
-const game = new Game(document.querySelector("#game"));
+class boatImage {
+    constructor() {
+        this.data = new Image();
+        this.data.src = "images/boat.png";
+        this.hasLoaded = false;
+        this.data.onload = this.onLoad.bind(this);
+    }
+
+    onLoad() {
+        this.hasLoaded = true;
+    }
+};
+
+const game = new Game(document.querySelector("#game"), new boatImage);
 game.runLoop();
